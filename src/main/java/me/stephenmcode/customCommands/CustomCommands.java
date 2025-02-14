27b222028cb7ebc42a2f4a62d958dca17668fca4
Stephen <93400341+StephenMCode@users.noreply.github.com>
2025-02-13 17:59:58 -0800

    Created separate command class for individual command, and created a god mode command

--- src/main/java/me/stephenmcode/customCommands/CustomCommands.java
+++ src/main/java/me/stephenmcode/customCommands/CustomCommands.java
@@ -1,15 +1,9 @@
 package me.stephenmcode.customCommands;
 
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.TextComponent;
-import net.kyori.adventure.text.format.TextColor;
-import org.bukkit.command.BlockCommandSender;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.command.ConsoleCommandSender;
-import org.bukkit.entity.Player;
+import me.stephenmcode.customCommands.commands.GodCommand;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
 
 public final class CustomCommands extends JavaPlugin {
 
@@ -18,40 +12,6 @@ public final class CustomCommands extends JavaPlugin {
 
         System.out.println("Custom Commands plugin by StephenMCode has started!");
 
-    }
-
-    @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
-
-        // /die - kills the player
-        if (command.getName().equalsIgnoreCase("die")) {
-
-            if (sender instanceof Player player) {
-
-                player.setHealth(0);
-
-                TextComponent message = Component.text("You have opted to die.")
-                                        .color(TextColor.color(174, 62, 62));
-                player.sendMessage(message);
-
-                return true;
-
-            } else if (sender instanceof ConsoleCommandSender) {
-
-                System.out.println("This command cannot be run by the console.");
-
-                return true;
-
-            } else if (sender instanceof BlockCommandSender) {
-
-                System.out.println("This command cannot be run by command blocks.");
-
-                return true;
-
-            }
-
-        }
-
-        return false;
+        Objects.requireNonNull(getCommand("god")).setExecutor(new GodCommand());
     }
 }
---
+++ src/main/java/me/stephenmcode/customCommands/commands/GodCommand.java
@@ -0,0 +1,43 @@
+package me.stephenmcode.customCommands.commands;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.format.TextColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class GodCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] strings) {
+
+        if (commandSender instanceof Player player) {
+
+            if (player.isInvulnerable()) {
+                player.setInvulnerable(false);
+
+                TextComponent message = Component.text("Godmode disabled.")
+                                .color(TextColor.color(145, 45, 39));
+
+                player.sendMessage(message);
+
+            } else {
+                player.setInvulnerable(true);
+
+                TextComponent message = Component.text("Godmode enabled.")
+                        .color(TextColor.color(59, 145, 70));
+
+                player.sendMessage(message);
+
+            }
+
+            return true;
+
+        }
+
+        return false;
+    }
+}
